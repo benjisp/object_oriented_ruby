c79620b3a7f21e27a98c1bc075c0e12f543b4e65
benji <benjisp018@gmail.com>
2019-08-20 14:07:48 -0400

    add object_oriented_ruby

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+candy1 = {:flavor => "chocolate", :price => 4000, :color => "white"}
+candy2 = {:flavor => "bubblegum", :price => 1000, :color => "pink"}
+candy3 = {:flavor => "lemon", :price => 2.5, :color => "blue"}
+
+candy1 = {flavor: "chocolate", :price 4000, :color "white"}
+candy2 = {flavor: "bubblegum", :price 1000, :color "pink"}
+candy3 = {flavor: "lemon", :price 2.5, :color "blue"}
